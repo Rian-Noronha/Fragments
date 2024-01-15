f6f2e122f9e54b3f2db76ab9232950f76a4edd69
Rian-Noronha <riannoronha011@gmail.com>
2024-01-15 12:55:35 -0300

    Initial commit

---
+++ app/src/main/java/com/rn/fragments/view/JogadorActivity.kt
@@ -0,0 +1,12 @@
+package com.rn.fragments.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rn.fragments.R
+
+class JogadorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jogador)
+    }
+}
